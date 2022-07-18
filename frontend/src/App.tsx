05311f6965254dcda1b1fc1789e9661266e7c550
Erick <erick1junior@hotmail.com>
2022-07-17 22:57:10 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
@@ -8,12 +9,10 @@ function App() {
       <main>
         <section id="sales">
           <div className="dsmeta-container">
-
+            <SalesCard />
           </div>
         </section>
       </main>
-      <NotificationButton />
-
     </>
   )
 }
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import './styles.css'
-import logo from '../../assets/img/logo.png'
+import logo from '../../assets/img/logo.svg'
 
 const Header = () => {
   return (
